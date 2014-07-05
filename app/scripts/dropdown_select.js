887ac34c7326f9e8b2e3a051d26c538e453393bb
Laura Patti <lpatti10@gmail.com>
2014-07-05 17:51:52 -0400

    Troubleshooting build problems

--- app/scripts/dropdown_select.js
+++
@@ -1,9 +0,0 @@
-$('select').on('change', function (){
-  
-  alert( $(this).val() );
-  
-  if ($(this).val() == 1){
-    // do something
-  }
-  
-});
--- app/scripts/hide_show.js
+++
@@ -1,5 +0,0 @@
-$('.d3').on('click', function() {
-  $(this).fadeOut('slow', function (){
-    $('.d2').fadeIn();
-  });
-});
